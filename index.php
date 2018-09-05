3299c28504761e4b1896249a6838b67ab24f9bf1
Elizabeth Limon <elimon@csumb.edu>
2018-09-05 04:42:34 +0000

    adding lab 1 files

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<html><body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Elizabeth's Web Portfolio</title>
+        </head>
+    <body>
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo 'Hello world from Cloud9!';
-
+//--------------------------------------------
+echo "<h1>Elizabeth Limon's CST336 site </h1>";
+<img src="https://comefruta.es/wp-content/uploads/limones_4.jpg">
 ?>
 </body>
 </html>
