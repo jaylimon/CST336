b1514c894140d6c66c2cee14d8f5f0a95ab67c40
Elizabeth Limon <elimon@csumb.edu>
2018-10-28 04:44:02 +0000

    new

--- inc/dbConnection.php
+++ inc/dbConnection.php
@@ -2,18 +2,19 @@
 
 function startConnection($dbname="ottermart") {
     //Creating database connection
-    $host = "";
+    $host = "localhost";
 //  $dbname = "ottermart";
     $username = "root";
     $password = "";
     
-    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("mysql://bb092d0f2db1ff:77f97df8@us-cdbr-iron-east-01.cleardb.net/heroku_dddcf515622bfb0?reconnect=true"));
-        $host = $url["us-cdbr-iron-east-01.cleardb.net"];
-        $dbname = substr($url["path"], 1);
-        $username = $url["bb092d0f2db1ff"];
-        $password = $url["77f97df8"];
-    } 
+    //checks whether the URL contains "herokuapp" (using Heroku)
+    if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+       $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+       $host = $url["host"];
+       $dbname = substr($url["path"], 1);
+       $username = $url["user"];
+       $password = $url["pass"];
+    }
     
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
