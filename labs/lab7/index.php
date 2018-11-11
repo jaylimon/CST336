27a2b7de38a4e5ed97986fcc5f872a4dc28ab9ff
Elizabeth Limon <elimon@csumb.edu>
2018-11-11 06:57:46 +0000

    add files

---
+++ labs/lab7/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Admin Login </title>
+    </head>
+     <style>
+     body{
+         background-color: #849A85;
+         text-align: center;
+     }
+        </style>
+    <body>
+
+        <h1> Ottermart - Admin Login </h1>
+        
+        <form method="post" action="loginProcess.php">
+          Username:  <input type="text" name="username"/> <br>
+          Password:  <input type="password" name="password"/> <br>
+          <input type="submit" value="Login">
+        </form>
+
+    </body>
+</html>
